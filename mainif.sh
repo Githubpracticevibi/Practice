b71920e2e5393ad5737c0c413dd2ff20922f7040
vibishanan <ec2-user@ip-172-31-0-110.ap-south-1.compute.internal>
2021-03-13 07:11:49 +0000

    prac

---
+++ mainif.sh
@@ -0,0 +1,22 @@
+echo "if condi starts"
+if [ -r $1 ]
+then
+echo " the file is reading"
+else 
+echo "file is not reading"
+fi
+
+if [ -w $1 ]
+then
+echo " the file is writing"
+else
+echo "file is not writing"
+fi
+
+if [ -x $1 ]
+then
+echo " the file is executing"
+else
+echo "file is not executing"
+fi
+
---
+++ while1.sh
@@ -0,0 +1,8 @@
+echo "the while function starts"
+read a
+while [ $a -gt 7 ]
+do
+echo "$a"	
+a=`expr $a - 1`
+done
+echo "It will not display"
